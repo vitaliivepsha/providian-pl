fd8bc8c96b01679d751e44bbb638501c88c8f682
Artem <nicktlibra@gmail.com>
2022-05-20 21:00:07 +0300

    gallery

--- app/app.js
+++ app/app.js
@@ -22,6 +22,9 @@ if (process.env.NODE_ENV !== 'production') {
     require('./assets/templates/layouts/catalog-list.html');
     require('./assets/templates/layouts/no-goods.html');
     require('./assets/templates/layouts/thank-you.html');
+    require('./assets/templates/layouts/photo-gallery.html');
+    require('./assets/templates/layouts/photo-gallery-category.html');
+    require('./assets/templates/layouts/video-gallery.html');
 }
 
 // Depends
@@ -124,8 +127,86 @@ $(function () {
     });
 
     // ===========================
-    // chechout
 
+    // photo slider navigation
+
+    $('.photo-slider').each(function () {
+        var $slider = $(this);
+
+        $slider.find('.slick-prev').append(`<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M8.00089 3.99966C8.00089 4.13226 7.94822 4.25944 7.85445 4.35321C7.76068 4.44698 7.6335 4.49966 7.50089 4.49966H1.70789L3.85489 6.64566C3.90138 6.69214 3.93826 6.74733 3.96342 6.80807C3.98858 6.86881 4.00152 6.93391 4.00152 6.99966C4.00152 7.0654 3.98858 7.1305 3.96342 7.19124C3.93826 7.25198 3.90138 7.30717 3.85489 7.35366C3.80841 7.40014 3.75322 7.43702 3.69248 7.46218C3.63174 7.48734 3.56664 7.50029 3.50089 7.50029C3.43515 7.50029 3.37005 7.48734 3.30931 7.46218C3.24857 7.43702 3.19338 7.40014 3.14689 7.35366L0.146894 4.35366C0.100331 4.30721 0.0633875 4.25203 0.0381811 4.19129C0.0129746 4.13054 0 4.06542 0 3.99966C0 3.93389 0.0129746 3.86877 0.0381811 3.80802C0.0633875 3.74728 0.100331 3.6921 0.146894 3.64566L3.14689 0.645655C3.24078 0.551769 3.36812 0.499023 3.50089 0.499023C3.63367 0.499023 3.76101 0.551769 3.85489 0.645655C3.94878 0.739542 4.00152 0.866879 4.00152 0.999655C4.00152 1.13243 3.94878 1.25977 3.85489 1.35366L1.70789 3.49966H7.50089C7.6335 3.49966 7.76068 3.55233 7.85445 3.6461C7.94822 3.73987 8.00089 3.86705 8.00089 3.99966Z" fill="white"/>
+</svg>
+`);
+
+        $slider.find('.slick-next').append(`<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M-0.000893899 4.00035C-0.00089391 3.86774 0.0517842 3.74056 0.145552 3.64679C0.23932 3.55302 0.366498 3.50035 0.499106 3.50035L6.29211 3.50034L4.14511 1.35435C4.09862 1.30786 4.06174 1.25267 4.03658 1.19193C4.01142 1.13119 3.99847 1.06609 3.99847 1.00035C3.99847 0.934601 4.01142 0.869501 4.03658 0.808761C4.06174 0.748022 4.09862 0.692833 4.14511 0.646345C4.19159 0.599857 4.24678 0.562981 4.30752 0.537822C4.36826 0.512663 4.43336 0.499714 4.49911 0.499714C4.56485 0.499714 4.62995 0.512663 4.69069 0.537822C4.75143 0.56298 4.80662 0.599857 4.85311 0.646345L7.85311 3.64634C7.89967 3.69279 7.93661 3.74797 7.96182 3.80871C7.98703 3.86946 8 3.93458 8 4.00034C8 4.06611 7.98703 4.13123 7.96182 4.19198C7.93661 4.25272 7.89967 4.3079 7.85311 4.35434L4.85311 7.35435C4.75922 7.44823 4.63188 7.50098 4.49911 7.50098C4.36633 7.50098 4.23899 7.44823 4.14511 7.35435C4.05122 7.26046 3.99848 7.13312 3.99848 7.00035C3.99848 6.86757 4.05122 6.74023 4.14511 6.64634L6.29211 4.50034L0.499106 4.50035C0.366498 4.50035 0.23932 4.44767 0.145552 4.3539C0.0517842 4.26013 -0.000893887 4.13295 -0.000893899 4.00035Z" fill="white"/>
+</svg>
+`);
+
+        $slider.closest('.counter-photo-slider__wrapper').find('.slider-next').click(function () {
+            $slider.slick('slickNext');
+        });
+
+        $slider.closest('.counter-photo-slider__wrapper').find('.slider-prev').click(function () {
+            $slider.slick('slickPrev');
+        });
+
+        var currentSlide;
+        var slidesCount;
+        var sliderCounter = $slider.closest('.counter-photo-slider__wrapper').find('.slider-counter');
+        $(sliderCounter).text('ФОТО ' + '1' + ' из ' + $slider.slick('getSlick').slideCount);
+
+        if ($slider.find('.slick-prev').hasClass('slick-disabled')) {
+            $slider.closest('.counter-photo-slider__wrapper').find('.slider-prev').addClass('slick-disabled');
+        }
+        else {
+            $slider.closest('.counter-photo-slider__wrapper').find('.slider-prev').removeClass('slick-disabled');
+        }
+
+        if ($slider.find('.slick-next').hasClass('slick-disabled')) {
+            $slider.closest('.counter-photo-slider__wrapper').find('.slider-next').addClass('slick-disabled');
+        }
+        else {
+            $slider.closest('.counter-photo-slider__wrapper').find('.slider-next').removeClass('slick-disabled');
+        }
+
+        var updateSliderCounter = function (slick, currentIndex) {
+            currentSlide = slick.slickCurrentSlide() + 1;
+            slidesCount = $slider.slick('getSlick').slideCount;
+            $(sliderCounter).text('Фото ' + currentSlide + ' из ' + slidesCount);
+        };
+
+        $slider.on('init', function (event, slick, slidesCount) {
+            updateSliderCounter(slick, slidesCount);
+        });
+
+
+        $slider.on('afterChange', function (event, slick, currentSlide) {
+            updateSliderCounter(slick, currentSlide);
+
+            if ($slider.find('.slick-prev').hasClass('slick-disabled')) {
+                $slider.closest('.counter-photo-slider__wrapper').find('.slider-prev').addClass('slick-disabled');
+            }
+            else {
+                $slider.closest('.counter-photo-slider__wrapper').find('.slider-prev').removeClass('slick-disabled');
+            }
+
+            if ($slider.find('.slick-next').hasClass('slick-disabled')) {
+                $slider.closest('.counter-photo-slider__wrapper').find('.slider-next').addClass('slick-disabled');
+            }
+            else {
+                $slider.closest('.counter-photo-slider__wrapper').find('.slider-next').removeClass('slick-disabled');
+            }
+        });
+    });
+
+    $('.popup-btn').on('click', function () {
+        $(window).trigger('resize');
+        $('.slick-slider').slick('setPosition');
+    });
+
+
+    // chechout
     // chechout goods del
 
     $('.checkout-goods__item-close').click(function () {
